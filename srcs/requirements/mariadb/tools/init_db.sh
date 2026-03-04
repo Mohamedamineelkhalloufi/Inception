c64d1a2d709d9f1edb3c6933aa5d0f51db92d1bc
Mohamedamineelkhalloufi <khalloufiamine2@gmail.com>
2026-03-04 22:59:50 +0000

    ERROR

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-service mariadb start
+if [ ! -d "/var/lib/mysql/mysql" ]; then
 
-mysql -u root << EOF
-CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-FLUSH PRIVILEGES;
-EOF
+    mysql_install_db --user=mysql --ldata=/var/lib/mysql
 
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+    mysqld --skip-networking --socket=/run/mysqld.sock &
+    pid="$!"
 
-exec mysqld_safe
+    sleep 5
+
+    mysql -u root << EOF
+    CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+    CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+    GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+    FLUSH PRIVILEGES;
+    EOF
+
+    kill $pid
+    wait $pid
+fi
+
+exec mysqld
--- srcs/requirements/wordpress/tools/wp_setup.sh
+++ srcs/requirements/wordpress/tools/wp_setup.sh
@@ -2,34 +2,32 @@
 
 set -e
 
-if [ ! -f /var/www/html/wp-load.php ]; then
+sleep 10
+
+if [ ! -f /var/www/html/wp-config.php ]; then
     wp core download --allow-root --path='/var/www/html'
-else
-{
-    rm -rf /var/www/html/*
-    wp core download --allow-root --path='/var/www/html'
-}
+
+    wp config create --allow-root \
+        --dbname=$MYSQL_DATABASE \
+        --dbuser=$MYSQL_USER \
+        --dbpass=$MYSQL_PASSWORD \
+        --dbhost=mariadb \
+        --path='/var/www/html'
+
+    wp core install \
+        --url=$WP_URL \
+        --title=$WP_TITLE \
+        --admin_user=$WP_AUSER \
+        --admin_password=$WP_APASSWORD \
+        --admin_email=$WP_AEMAIL \
+        --allow-root \
+        --path='/var/www/html'
+
+    wp user create $NEW_USER $EMAIL_USER \
+        --role=editor \
+        --user_pass=$USER_PASS \
+        --allow-root \
+        --path='/var/www/html'
 fi
-
-wp config create --allow-root \
-    --dbname=$MYSQL_DATABASE \
-    --dbuser=$MYSQL_USER \
-    --dbpass=$MYSQL_PASSWORD \
-    --dbhost=mariadb \
-    --path='/var/www/html'
-
-wp core install \
-    --url=$WP_URL \
-    --title=$WP_TITLE \
-    --admin_user=$WP_AUSER \
-    --admin_password=$WP_APASSWORD \
-    --admin_email=$WP_AEMAIL \
-    --allow-root \
-    --path='/var/www/html'
-wp user create $NEW_USER $EMAIL_USER \
-    --role=editor \
-    --user_pass=$USER_PASS \
-    --allow-root \
-    --path='/var/www/html'
 
 exec php-fpm8.4 -F
